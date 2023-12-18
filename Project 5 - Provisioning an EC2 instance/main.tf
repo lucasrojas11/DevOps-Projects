ed8a6bf05384e9ff92b55f70b63c1d7b132c1fa0
lucas rojas <lucasrojas1066@gmail.com>
2023-12-18 20:26:30 -0300

    adding scripts

---
+++ Project 5 - Provisioning an EC2 instance/main.tf
@@ -0,0 +1,45 @@
+resource "aws_instance" "web" {
+  ami                    = "ami-0f5ee92e2d63afc18"   #change ami id for different region
+  instance_type          = "t2.large"
+  key_name               = "Mumbai"
+  vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
+  user_data              = templatefile("./install.sh", {})
+
+  tags = {
+    Name = "Jenkins-sonarqube"
+  }
+
+  root_block_device {
+    volume_size = 30
+  }
+}
+
+resource "aws_security_group" "Jenkins-sg" {
+  name        = "Jenkins-sg"
+  description = "Allow TLS inbound traffic"
+
+  ingress = [
+    for port in [22, 80, 443, 8080, 9000] : {
+      description      = "inbound rules"
+      from_port        = port
+      to_port          = port
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }
+  ]
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "jenkins-sg"
+  }
+}
